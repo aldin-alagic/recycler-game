bbca92dcb84f0ca49b4c9e36e5cfc94b31b41801
aalagic <aldin_alagic@hotmail.com>
2020-03-01 23:19:54 +0100

    Create C# script for movement of the garbage truck
    Add C# script which will implement the functionality of moving the garbage truck. This will be used in the movement of the garbage truck. Specifically, moving the truck from one to other waypoint.

---
+++ Assets/Scripts/Truck.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+public class Truck : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private Transform target;
+    private int wavepointIndex = 0;
+
+    private void Start()
+    {
+        target = Waypoints.points[0];
+    }
+
+    private void Update()
+    {
+        Vector3 direction = target.position - transform.position;
+        transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+        transform.forward = Vector3.Normalize(direction);
+        if (Vector3.Distance(transform.position, target.position) <= 0.4f)
+        {
+            GetNextWaypoint();
+        }
+    }
+
+    private void GetNextWaypoint()
+    {
+        if (wavepointIndex >= Waypoints.points.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        wavepointIndex++;
+        target = Waypoints.points[wavepointIndex];
+    }
+}
