f505ca789c638d437bbc110a4d47587bf2f66ed5
aalagic <aldin_alagic@hotmail.com>
2020-03-02 19:37:04 +0100

    Create C# script for movement of the camera
    Add C# script which will implement the functionality of moving the camera. This will be used to make the camera follow the truck while its moving.

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform target;
+    public float sSpeed = 10.0f;
+    public Vector3 dist;
+    public Transform lookTarget;
+
+    void LateUpdate()
+    {
+        Vector3 dPos = target.position + dist;
+        Vector3 sPos = Vector3.Lerp(transform.position, dPos, sSpeed * Time.deltaTime);
+        transform.position = sPos;
+        transform.LookAt(lookTarget.position);
+    }
+
+}
+
+
