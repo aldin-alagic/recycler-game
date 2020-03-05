019c1277ab3f6793d15c76d6a956fd59dd78c165
aalagic <aldin_alagic@hotmail.com>
2020-03-05 14:27:04 +0100

    Create C# script for implementing an audio manager
    Add C# script which will implement the functionality of having a audio manager which will handle playing sounds in the game. Audio manager has an array of sounds and methods to play these sounds from other C# scripts.

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public static AudioManager instance;
+
+    public AudioMixerGroup mixerGroup;
+
+    public Sound[] sounds;
+
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
+        DontDestroyOnLoad(gameObject);
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.loop = s.loop;
+            s.source.outputAudioMixerGroup = s.mixerGroup;
+        }
+    }
+
+    public void Play(string _sound)
+    {
+        Sound sound = PrepareSound(_sound);
+        sound.source.Play();
+    }
+
+    public void PlayDelayed(string _sound, float delay)
+    {
+        Sound sound = PrepareSound(_sound);
+        sound.source.PlayDelayed(delay);
+    }
+
+    private Sound PrepareSound(string _sound)
+    {
+        Sound sound = Array.Find(sounds, item => item.name == _sound);
+        sound.source.volume = sound.volume * (1f + UnityEngine.Random.Range(-sound.volumeVariance / 2f, sound.volumeVariance / 2f));
+        sound.source.pitch = sound.pitch * (1f + UnityEngine.Random.Range(-sound.pitchVariance / 2f, sound.pitchVariance / 2f));
+        return sound;
+    }
+
+    public void Stop(string _sound)
+    {
+        Sound sound = Array.Find(sounds, item => item.name == _sound);
+        if (sound == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+        sound.source.Stop();
+    }
+}
