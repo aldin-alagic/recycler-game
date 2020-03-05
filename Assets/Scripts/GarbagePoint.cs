f4e7f5fd6d4d61d2e269decfacbfe6ed4dd7e9db
aalagic <aldin_alagic@hotmail.com>
2020-03-05 02:55:25 +0100

    Simplify C# script for implementing GarbagePoint's recycling
    Simplify the code of C# script which will implement the functionality of opening a recycle dialog when garbage truck is close to the GarbagePoint .

--- Assets/Scripts/GarbagePoint.cs
+++ Assets/Scripts/GarbagePoint.cs
@@ -7,12 +7,7 @@ public class GarbagePoint : MonoBehaviour
     public float range = 2f;
     public string garbageTruckTag = "Garbage Truck";
     public bool cleared = false;
-    public Texture2D garbageImage;
     public GameObject recycleDialogUI;
-    public GameObject imageContainer;
-    public GameObject textContainer;
-    public GameObject correctGarbageCanBtn;
-    public string explanation = "Vrećice grickalica idu u kanticu za papir";
 
     void Start()
     {
@@ -25,12 +20,9 @@ public class GarbagePoint : MonoBehaviour
         float distanceGarbageTruck = Vector3.Distance(transform.position, garbageTruck.transform.position);
         if (distanceGarbageTruck <= range && !cleared)
         {
-            imageContainer.GetComponentInChildren<RawImage>().texture = garbageImage;
-            textContainer.GetComponentInChildren<Text>().text = explanation;
-            correctGarbageCanBtn.SetActive(false);
             recycleDialogUI.SetActive(true);
             cleared = true;
-            Destroy(gameObject);
+            Destroy(gameObject, 0.1f);
         }
     }
 
