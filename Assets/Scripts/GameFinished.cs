0f8eb1e7a5b5b02a0163e1607faed2157b301a6e
aalagic <aldin_alagic@hotmail.com>
2020-03-08 00:31:37 +0100

    Create C# script for implementing an game finished menu
    Add C# script which will implement the functionality of having a game finished menu. This menu will show player stats and buttons to start a new game or exit the game.

---
+++ Assets/Scripts/GameFinished.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameFinished : MonoBehaviour
+{
+
+    public Text correctAnswersValue;
+    public Text incorrectAnswersValue;
+    public Text timeValue;
+    public Text scoreValue;
+    public Text bestScoreValue;
+    public SceneTransitionManager sceneTransition;
+
+    private AudioManager audioManager;
+
+    public void Retry()
+    {
+        Time.timeScale = 1f;
+        sceneTransition.FadeTo(SceneManager.GetActiveScene().name);
+    }
+
+    public void Exit()
+    {
+        Time.timeScale = 1f;
+        Application.Quit();
+    }
+
+    void OnEnable()
+    {
+        audioManager = AudioManager.instance;
+        audioManager.Stop("Driving");
+        Time.timeScale = 0f;
+        PlayerStatistics.gameTime = Time.realtimeSinceStartup;
+        PlayerStatistics.timeScore = 6000f - (PlayerStatistics.gameTime * 10);
+        PlayerStatistics.playerScore += PlayerStatistics.timeScore;
+
+        correctAnswersValue.text = PlayerStatistics.correctAnswers.ToString();
+        incorrectAnswersValue.text = PlayerStatistics.incorrectAnswers.ToString();
+        timeValue.text = PlayerStatistics.gameTime.ToString("F2") + "  s";
+        scoreValue.text = PlayerStatistics.playerScore.ToString("F2");
+        bestScoreValue.text = PlayerStatistics.playerScore.ToString("F2");
+    }
+}
+
---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PlayerStatistics : MonoBehaviour
+{
+
+    public static int correctAnswers;
+    public static int incorrectAnswers;
+    public static float gameTime;
+    public static float timeScore = 10000f;
+    public static float playerScore;
+
+
+    void Start()
+    {
+        gameTime = 0;
+        correctAnswers = 0;
+        incorrectAnswers = 0;
+        playerScore = 0f;
+    }
+
+
+}
