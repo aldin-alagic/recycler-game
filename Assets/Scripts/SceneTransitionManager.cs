3acea3b8fbc6f126d3e29336fc31898ba3ee5439
aalagic <aldin_alagic@hotmail.com>
2020-03-08 00:16:30 +0100

    Create C# script for implementing switching and fadeing between scenes
    Add C# script which will implement the functionality of switching and slowly fadeing from and fadeing to scenes. This script will be atached to SceneManager object.

---
+++ Assets/Scripts/SceneTransitionManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SceneTransitionManager : MonoBehaviour
+{
+    public Image image;
+    public AnimationCurve curve;
+
+    void Start()
+    {
+        StartCoroutine(FadeIn());
+    }
+
+    IEnumerator FadeIn()
+    {
+        float time = 1f;
+
+        while (time > 0f)
+        {
+            time -= Time.deltaTime * 0.5f;
+            float alpha = curve.Evaluate(time);
+            image.color = new Color(0f, 0f, 0f, time);
+            yield return 0;
+        }
+    }
+    public void FadeTo(string scene)
+    {
+        StartCoroutine(FadeOut(scene));
+    }
+
+    IEnumerator FadeOut(string scene)
+    {
+        float time = 0f;
+
+        while (time < 1f)
+        {
+            time += Time.deltaTime * 0.5f;
+            float alpha = curve.Evaluate(time);
+            image.color = new Color(0f, 0f, 0f, time);
+            yield return 0;
+        }
+        SceneManager.LoadScene(scene);
+    }
+}
