42c45f8dc1839f650ab24dd2b36cd7e561b425ac
aalagic <aldin_alagic@hotmail.com>
2020-03-07 22:51:43 +0100

    Create recycle dialog animations
    Add a animation and animation controller for recycle dialog. This animation will make the dialog and its child objects fade in.

--- Assets/Scripts/RecyclerDialog.cs
+++ Assets/Scripts/RecyclerDialog.cs
@@ -19,9 +19,20 @@ public class RecyclerDialog : MonoBehaviour
     private AudioManager audioManager;
     private Garbage selectedGarbage;
     private int randomIndex;
+    private bool firstTime = true;
 
     private void OnEnable()
     {
+        if (firstTime)
+        {
+            foreach (var btn in garbageCanBtns)
+            {
+                btn.GetComponentInChildren<Button>().onClick.AddListener(WrongAnswer);
+            }
+
+            firstTime = false;
+        }
+
         audioManager = AudioManager.instance;
         Pause();
 
@@ -31,7 +42,9 @@ public class RecyclerDialog : MonoBehaviour
             selectedGarbage = garbages[randomIndex];
         } while (selectedGarbage.cleared);
 
+        garbageCanBtns[selectedGarbage.garbageCan].GetComponentInChildren<Button>().onClick.RemoveAllListeners();
         garbageCanBtns[selectedGarbage.garbageCan].GetComponentInChildren<Button>().onClick.AddListener(CorrectAnswer);
+
         textContainer.GetComponent<TMPro.TextMeshProUGUI>().text = selectedGarbage.text;
         nameContainer.GetComponent<TMPro.TextMeshProUGUI>().text = selectedGarbage.name;
         imageContainer.GetComponent<UnityEngine.UI.RawImage>().texture = selectedGarbage.image;
@@ -58,8 +71,8 @@ public class RecyclerDialog : MonoBehaviour
         textContainer.SetActive(true);
         selectedGarbage.cleared = true;
         audioManager.Play("Incorrect");
-        audioManager.Stop("Correct");
-
+        PlayerStatistics.incorrectAnswers++;
+        Debug.Log("Pogresan");
         StartCoroutine(WaitForReading());
     }
 
@@ -71,10 +84,9 @@ public class RecyclerDialog : MonoBehaviour
         textContainer.SetActive(true);
         selectedGarbage.cleared = true;
         audioManager.Play("Correct");
-        audioManager.Stop("Incorrect");
-
-
-
+        PlayerStatistics.correctAnswers++;
+        PlayerStatistics.playerScore += 1000;
+        Debug.Log("Tocan");
         StartCoroutine(WaitForReading());
     }
 
@@ -85,6 +97,7 @@ public class RecyclerDialog : MonoBehaviour
         garbageCans.SetActive(true);
         resultTextContainer.GetComponent<UnityEngine.UI.Text>().color = Color.white;
         resultTextContainer.GetComponent<UnityEngine.UI.Text>().text = titleText;
+        garbageCanBtns[selectedGarbage.garbageCan].GetComponentInChildren<Button>().onClick.RemoveAllListeners();
         garbageCanBtns[selectedGarbage.garbageCan].GetComponentInChildren<Button>().onClick.AddListener(WrongAnswer);
         textContainer.SetActive(false);
         UI.SetActive(false);
@@ -93,13 +106,13 @@ public class RecyclerDialog : MonoBehaviour
 
     IEnumerator WaitForReading()
     {
-        yield return new WaitForSecondsRealtime(4);
+        yield return new WaitForSecondsRealtime(1);
         exitBtn.SetActive(true);
     }
 
     IEnumerator Wait()
     {
-        yield return new WaitForSecondsRealtime(2);
+        yield return new WaitForSecondsRealtime(1);
         UI.SetActive(true);
     }
 }
