b600d397bf135bcbf3f2ef46deb140c75b6e1214
aalagic <aldin_alagic@hotmail.com>
2020-03-10 01:21:59 +0100

    Create C# classes for players score
    Add C# classes Score and Scores which will will be used to store players score prior to saving the score in json format. Scores class is used for list of Score objects.

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,10 @@
+﻿[System.Serializable]
+public class Score
+{
+    public float value;
+
+    public Score(float _value)
+    {
+        value = _value;
+    }
+}
---
+++ Assets/Scripts/Scores.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+[System.Serializable]
+public class Scores
+{
+    public List<Score> scores;
+
+    public Scores()
+    {
+        scores = new List<Score>();
+    }
+}
