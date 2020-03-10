49da0a40239769eeead1040f3a57173409c1269e
aalagic <aldin_alagic@hotmail.com>
2020-03-10 01:36:00 +0100

    Add a method for reseting players stats in PlayerStats class
    Add a method which which will be used for reseting players stats at the beginning of each game. This will stop old stats from interfering with stats from the new game.

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -6,17 +6,21 @@ public class PlayerStatistics : MonoBehaviour
     public static int correctAnswers;
     public static int incorrectAnswers;
     public static float gameTime;
-    public static float timeScore = 10000f;
+    public static float timeScore;
     public static float playerScore;
 
-
-    void Start()
+    void Awake()
     {
-        gameTime = 0;
+        ResetStats();
+    }
+
+    public static void ResetStats()
+    {
+        gameTime = Time.realtimeSinceStartup;
         correctAnswers = 0;
         incorrectAnswers = 0;
         playerScore = 0f;
+        timeScore = 10000f;
     }
-
 
 }
