939de3243b9df86624a108e14d573dec519b83f1
aalagic <aldin_alagic@hotmail.com>
2020-03-10 01:43:29 +0100

    Add score saving and best score loading in GameFinished class
    Add score saving when game finished menu opens, and then load the best score and display it.

--- Assets/Scripts/GameFinished.cs
+++ Assets/Scripts/GameFinished.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
@@ -13,33 +14,42 @@ public class GameFinished : MonoBehaviour
     public SceneTransitionManager sceneTransition;
 
     private AudioManager audioManager;
+    private SaveData saveData;
 
     public void Retry()
     {
+        PlayerStatistics.ResetStats();
         Time.timeScale = 1f;
         sceneTransition.FadeTo(SceneManager.GetActiveScene().name);
     }
 
     public void Exit()
     {
+        PlayerStatistics.ResetStats();
         Time.timeScale = 1f;
         Application.Quit();
     }
 
     void OnEnable()
     {
+        saveData = SaveData.instance;
         audioManager = AudioManager.instance;
         audioManager.Stop("Driving");
         Time.timeScale = 0f;
-        PlayerStatistics.gameTime = Time.realtimeSinceStartup;
+
+        PlayerStatistics.gameTime = Time.realtimeSinceStartup - PlayerStatistics.gameTime;
         PlayerStatistics.timeScore = 6000f - (PlayerStatistics.gameTime * 10);
         PlayerStatistics.playerScore += PlayerStatistics.timeScore;
+        Score score = new Score(PlayerStatistics.playerScore);
+        saveData.Save(score);
 
         correctAnswersValue.text = PlayerStatistics.correctAnswers.ToString();
         incorrectAnswersValue.text = PlayerStatistics.incorrectAnswers.ToString();
         timeValue.text = PlayerStatistics.gameTime.ToString("F2") + "  s";
         scoreValue.text = PlayerStatistics.playerScore.ToString("F2");
-        bestScoreValue.text = PlayerStatistics.playerScore.ToString("F2");
+
+        float bestScore = saveData.Load().Max(sc => sc.value);
+        bestScoreValue.text = bestScore.ToString("F2");
     }
 }
 
