cac2329a55d5c1ae68f6f66e076b2f6c8071aaae
aalagic <aldin_alagic@hotmail.com>
2020-03-10 01:30:31 +0100

    Create C# script for saving and loading players scores
    Add C# script which will implement the functionality of saving and loading players scores. This script will take players score at the end of the the game and save it and then load players best score.

---
+++ Assets/Scripts/SaveData.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class SaveData : MonoBehaviour
+{
+    public static SaveData instance;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+    public void Save(Score score)
+    {
+        List<Score> scores = new List<Score>();
+        if (PlayerPrefs.HasKey("Data"))
+        {
+            scores = Load();
+        }
+        scores.Add(score);
+
+        string json = JsonHelper.ToJson(scores.ToArray());
+        PlayerPrefs.DeleteKey("Data");
+        PlayerPrefs.SetString("Data", json);
+        PlayerPrefs.Save();
+    }
+
+    public List<Score> Load()
+    {
+        string json = PlayerPrefs.GetString("Data");
+        Score[] scoresArr = JsonHelper.FromJson<Score>(json);
+        List<Score> scores = scoresArr.OfType<Score>().ToList();
+
+        return scores;
+    }
+
+}
